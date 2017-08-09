308db2f8bf8d2b6feb4818eff66fb91f770f6fd0
Avleen <avleen3@gmail.com>
2017-08-09 20:20:53 +0000

    define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ $(document).ready(function){
+  function createTweetElement(dataObj) {
+  var $tweet = $("<article>").addClass("user-article");
+ // create header and its children
+  var header = $("<header>").addClass("user-tweet-header");
+
+    header.append($("<img>").addClass("user-thumbnail").attr("src", dataObj.user.avatars.small));
+    header.append($("<h2>").addClass("user-name").text(dataObj.user.name));
+    header.append($("<p>").addClass("user-handle").text(dataObj.user.handle));
+
+ // create main and its children
+  var main = $("<main>").addClass("user-feed");
+    main.append($("<p>")).addClass("tweet-body");
+
+ // create footer and its children
+  var footer = $("<footer>").addClass("user-tweet-footer");
+
+  var date = moment(dataObj.created_at);
+    footer.append($("<span>").addClass("tweet-timestamp").text(date.fromNow()));
+    footer.append($("<i>").addClass("fa fa-heart"));
+    footer.append($("<i>").addClass("fa fa-retweet"));
+    footer.append($("<i>").addClass("fa fa-flag"));
+ // append header, main , footer to tweet
+    $tweet.append(header).append(main).append(footer);
+    return $tweet;
+  }
+});
