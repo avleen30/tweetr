78d8b4c795041aaf5e617447c5e7f361e335a4f7
Avleen <avleen3@gmail.com>
2017-08-09 17:58:09 +0000

    CSS Styling for user-tweets feed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,7 @@
     var newcounter = 140 - $(this).val().length;
     $(".counter").html(newcounter);
 
+    //to change color of character length displayed
     if (newcounter < 0){
       $(".counter").addClass('error');
       } else {
