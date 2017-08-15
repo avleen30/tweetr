9a3452f6d4da782fec42ba33434e812d5fcff1c4
Avleen <avleen3@gmail.com>
2017-08-15 00:03:52 +0000

    proper indention for app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,30 +5,31 @@
  */
  $( document ).ready(function(){
    function createTweetElement(dataObj) {
-    var $tweet = $("<article>").addClass("user-article");
- // create header and its children
- var header = $("<header>").addClass("user-tweet-header");
- header.append($("<img>").addClass("user-thumbnail").attr("src", dataObj.user.avatars.small));
- header.append($("<h2>").addClass("user-name").text(dataObj.user.name));
- header.append($("<p>").addClass("user-handle").text(dataObj.user.handle));
+      var $tweet = $("<article>").addClass("user-article");
+      // create header and its children
+      var header = $("<header>").addClass("user-tweet-header");
+      header.append($("<img>").addClass("user-thumbnail").attr("src", dataObj.user.avatars.small));
+      header.append($("<h2>").addClass("user-name").text(dataObj.user.name));
+      header.append($("<p>").addClass("user-handle").text(dataObj.user.handle));
 
- // create main and its children
- var main = $("<main>").addClass("user-feed");
- main.append($("<p>").addClass("tweet-body").text(dataObj.content.text));
+      // create main and its children
+      var main = $("<main>").addClass("user-feed");
+      main.append($("<p>").addClass("tweet-body").text(dataObj.content.text));
 
- // create footer and its children
- var footer = $("<footer>").addClass("user-tweet-footer");
+      // create footer and its children
+      var footer = $("<footer>").addClass("user-tweet-footer");
 
- var date = moment(dataObj.created_at);
- footer.append($("<span>").addClass("tweet-timestamp").text(date.fromNow()));
- footer.append($("<i>").addClass("fa fa-heart"));
- footer.append($("<i>").addClass("fa fa-retweet"));
- footer.append($("<i>").addClass("fa fa-flag"));
- // append header, main , footer to tweet
- $tweet.append($(header)).append($(main)).append($(footer));
+      var date = moment(dataObj.created_at);
+      footer.append($("<span>").addClass("tweet-timestamp").text(date.fromNow()));
+      footer.append($("<i>").addClass("fa fa-heart"));
+      footer.append($("<i>").addClass("fa fa-retweet"));
+      footer.append($("<i>").addClass("fa fa-flag"));
+      // append header, main , footer to tweet
+      $tweet.append($(header)).append($(main)).append($(footer));
 
- return $tweet;
+  return $tweet;
 }
+
 var selector = '#tweets-container'
 
 //to take in an array of tweet objects and then append each one to the #tweets-container
